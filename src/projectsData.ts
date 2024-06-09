165fa47321185f45651958225ad4531f49e9a02d
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-06-09 02:43:15 -0300

    added projects to portfolio

---
+++ src/projectsData.ts
@@ -0,0 +1,18 @@
+// projectsData.ts
+const projects = [
+	{
+	  id: 1,
+	  name: "Prompt Library",
+	  description: "Project made to share AI prompts",
+	  html_url: "https://prompt-library-six.vercel.app/"
+	},
+	{
+	  id: 2,
+	  name: "Youtube Clone",
+	  description: "Youtube clone made using Vite",
+	  html_url: "https://yourvideomedia.netlify.app/"
+	},
+	// Add more projects as needed
+  ];
+
+export default projects;
