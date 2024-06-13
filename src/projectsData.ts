3e8c29ab6216a0c277572ca328a2e519c6011249
Manu-Martinez <manu.martinezgalan@gmail.com>
2024-06-13 01:38:23 -0300

    added tags

--- src/projectsData.ts
+++ src/projectsData.ts
@@ -3,15 +3,25 @@ const projects = [
 	{
 	  id: 1,
 	  name: "Prompt Library",
+	  tags: "AI, Prompting, React, Vite",
 	  description: "Project made to share AI prompts",
 	  html_url: "https://prompt-library-six.vercel.app/"
 	},
 	{
 	  id: 2,
 	  name: "Youtube Clone",
+	  tags: "AI, Prompting, React, Vite",
 	  description: "Youtube clone made using Vite",
 	  html_url: "https://yourvideomedia.netlify.app/"
 	},
+	{
+		id: 3,
+		name: "TextToIkon",
+		tags: "AI, Prompting, React, Vite",
+		description: "Main website of the swiss start-up, KomunIkon",
+		html_url: "https://texttoikon.komunikon.com/"
+	  },
+
 	// Add more projects as needed
   ];
 
